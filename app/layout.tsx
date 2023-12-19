e21c143b24bd896246be27d329d1ef118e1dde53
Sunny Eyles <sunnyeyles@Sunnys-MacBook-Air.local>
2023-12-19 14:41:30 +0100

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Sunny Portfolio',
+  title: 'sunny',
   description: 'View all my Works',
 }
 
